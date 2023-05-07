db37dbbf9e1497b672db019d5fe732694ce6ca29
YuvalDahari <yuvaldahari71568@gmail.com>
2023-05-07 17:11:02 +0300

    adding logics for register page

--- src/registerForm/RegisterForm.js
+++ src/registerForm/RegisterForm.js
@@ -1,9 +1,9 @@
 import FormItem from "../formItem/FormItem.js"
 
-function RegisterForm({ type: formType, labelText: labelText, inputText: inputText }) {
+function RegisterForm({ type: formType, labelText: labelText, inputText: inputText, value: value, onChange: onChange, onKeyUp: onKeyUp }) {
     return (
         <div className="col-md-6 mb-3">
-            <FormItem type={formType} labelText={labelText} inputText={inputText} />
+            <FormItem type={formType} labelText={labelText} inputText={inputText} value={value} onChange={onChange} onKeyUp={onKeyUp} />
         </div>
     );
 }
