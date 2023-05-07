ffaf882f0029a3c50675d60051618c36e1c44ce3
YuvalDahari <yuvaldahari71568@gmail.com>
2023-05-07 17:14:09 +0300

    some changes

--- src/formItem/FormItem.js
+++ src/formItem/FormItem.js
@@ -1,11 +1,11 @@
-function FormItem({type: formType, labelText: labelText, inputText: inputText}) {
+function FormItem({ type: formType, labelText: labelText, inputText: inputText, value: value, onChange: onChange, onKeyUp: onKeyUp }) {
     return (
         <div className="row">
             <div className="col-12">
                 <label className="form-label field">{labelText}</label>
             </div>
             <div className="col-12">
-                <input type={formType} className="form-control" placeholder={inputText}></input>
+                <input type={formType} className="form-control" placeholder={inputText} value={value} onChange={onChange} onKeyUp={onKeyUp} required></input>
             </div>
         </div>
     );
